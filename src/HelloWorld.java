4eba52e10a5fc4356e729f9848a02423783c43cd
Daniel Wanderley de Barros Correia Fitipaldi <1192306314@acd.grupotiradentes.com>
2020-02-10 19:15:01 -0300

    pedindo nome ao usuário

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Scanner leTeclado = new Scanner(System.in);
+		
+		System.out.println("Digite o seu nome: ");
+		
+		String nome = leTeclado.next();
+		
+		System.out.println("Hello, " + nome + "!" );
+		
 
 	}
 
